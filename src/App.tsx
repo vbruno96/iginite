730a8366481d90ebe4abf3b7e225c78421b192c7
Bruno Vinícius <vbruno96@gmail.com>
2023-02-24 17:17:43 -0300

    create sidebar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { Header } from "./components/Header"
+import styles from './assets/css/App.module.css'
+import { Sidebar } from "./components/Sidebar"
 
 function App() {
 
   return (
     <div className="App">
       <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>Teste</main>
+      </div>
     </div>
   )
 }
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,29 @@
+import { PencilLine } from 'phosphor-react'
+import styles from '../assets/css/Sidebar.module.css'
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1624696941338-934bf86c28b4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+      />
+
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://github.com/vbruno96.png"
+        />
+        <strong>Bruno Vinícius</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20}/>
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  )
+}
