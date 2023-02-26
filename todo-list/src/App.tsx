fce42fb32f019b6f03d944c04165d15eac11bcce
Bruno Vinícius <vbruno96@gmail.com>
2023-02-26 01:28:25 -0300

    create form new todo

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -1,3 +1,5 @@
+import { ChangeEvent, useState } from "react"
+import { PlusCircle } from "phosphor-react"
 import { Header } from "./components/Header"
 
 interface TypeTodos {
@@ -7,10 +9,37 @@ interface TypeTodos {
 }
 
 function App() {
+  const [newTodo, setNewTodo] = useState('')
+  const [todos, setTodos] = useState([] as TypeTodos[])
+
+  function handleNewTodoInputChange(event: ChangeEvent<HTMLInputElement>) {
+    setNewTodo(event.target.value)
+  }
 
   return (
     <>
       <Header />
+      <div className="content">
+        <form>
+          <input
+            type="text"
+            placeholder="Adicione uma nova tarefa"
+            value={newTodo}
+            onChange={handleNewTodoInputChange}
+          />
+          <button type="submit">Criar <PlusCircle weight="bold"/></button>
+        </form>
+        <div className="header-todos">
+          <div className="created-todos">
+            <strong>Tarefas criadas</strong>
+            <span>0</span>
+          </div>
+          <div className="completed-todos">
+            <strong>Concluídas</strong>
+            <span>0</span>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
